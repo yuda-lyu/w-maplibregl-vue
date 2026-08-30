514f681a175087159e9d93e5bee525f9e2d009be
yuda-lyu <firsemisphere@gmail.com>
2026-08-30 18:52:40 +0800

    modify: refa. files

--- src/js/defTerrainMap.mjs
+++ src/js/defTerrainMap.mjs
@@ -1,3 +1,7 @@
+/**
+ * defTerrainMap.mjs
+ * 預設3D地形資料源設定, 供組件預設值使用, 亦可由外部直接引用或作為自訂設定之基礎
+ */
 let defTerrainMap = {
     terrainSource: {
         url: '//s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png',
